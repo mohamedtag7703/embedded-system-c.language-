7ed7a8dc9d14f56360ddef5e32ae610d25f2bfea
mohamedtag7703 <69688624+mohamedtag7703@users.noreply.github.com>
2021-04-15 19:58:50 +0300

    Add files via upload

---
+++ lap3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(void)
+{
+	int x=0,y=0,z=0;
+	printf("please enter 3 integers");
+	scanf("%d",&x);
+	scanf("%d",&y);
+	scanf("%d",&z);
+	printf("the first integer =%d\n" ,x);
+	printf("the second integer =%d\n" ,y);
+	printf("the third integer =%d\n" ,z);
+}
